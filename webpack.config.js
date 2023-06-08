144b6103302723e1c189150d2e4e1fc6a73bd8eb
Dachrono <bluefire_andy@hotmail.com>
2023-06-07 23:15:17 -0600

    automatic server changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
         index: './src/index.js',
         print: './src/print.js',
       },
+      devtool: 'inline-source-map',
       plugins: [
         new HtmlWebpackPlugin({
          template: './src/index.html',
@@ -15,6 +17,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
   module: {
     rules: [
