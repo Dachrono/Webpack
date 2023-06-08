bbda49428ba32b2327b674cb80e0a7a3c95a4433
Dachrono <bluefire_andy@hotmail.com>
2023-06-07 22:23:56 -0600

    HtmlWebPackPlugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button')
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'click me and chec the console!'
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
   
     return element;
   }
